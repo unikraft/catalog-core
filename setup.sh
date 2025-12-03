253701bf704e410b387d562683614cc3286a8ed0
Rares-Stefan Goidescu <known.as.rares@gmail.com>
2025-12-03 11:07:57 +0200

    setup: Fix test path for lib-redis
    Change test path from `repos/apps/redis` to `repos/libs/redis` to
    properly detect if the `lib-redis` repository already exists in the
    correct location.
    
    Signed-off-by: Rares-Stefan Goidescu <known.as.rares@gmail.com>

--- setup.sh
+++ setup.sh
@@ -12,5 +12,5 @@ test -d repos/libs/python3 || git clone https://github.com/unikraft/lib-python3 
 test -d repos/libs/click || git clone https://github.com/unikraft/lib-click.git repos/libs/click
 test -d repos/libs/libelf || git clone https://github.com/unikraft/lib-libelf repos/libs/libelf
 test -d repos/apps/elfloader || git clone https://github.com/unikraft/app-elfloader repos/apps/elfloader
-test -d repos/apps/redis || git clone https://github.com/unikraft/lib-redis repos/libs/redis
+test -d repos/libs/redis || git clone https://github.com/unikraft/lib-redis repos/libs/redis
 test -d repos/libs/wamr || git clone https://github.com/unikraft/lib-wamr repos/libs/wamr
