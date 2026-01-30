7949a945efd58d2d4c2aff00e34e038b11ce57f9
Tushar <104261506+vTusharr@users.noreply.github.com>
2026-01-30 22:49:27 +0530

    Apply suggestions from code review
    Co-authored-by: Cezar Craciunoiu <cezar.craciunoiu@gmail.com>
    Signed-off-by: Tushar <104261506+vTusharr@users.noreply.github.com>

--- zig-hello/hello.zig
+++ zig-hello/hello.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 pub export fn main(argc: c_int, argv: [*c][*c]u8) c_int {
     _ = argc; _ = argv;
-    _ = std.c.printf("Hello from Zig !\n");
+    _ = std.c.printf("Hello from Zig!\n");
     return 0;
 }
